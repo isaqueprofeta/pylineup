43ff37462dd09f64fa4ec5fb9668f1828c470c5d
Isaque Profeta <isaqueprofeta@gmail.com>
2021-09-09 23:06:13 +0000

    Updated versions (Python 3.9 and Celery 6)

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.6-alpine
+FROM python:3-alpine
 
 RUN apk add --no-cache tzdata
 ENV TZ America/Sao_Paulo
--- pylineup/config.py
+++ pylineup/config.py
@@ -7,13 +7,13 @@ class Config:
     config_file = configparser.ConfigParser()
     config_file.read("config.ini")
 
-    BROKER_URL = config_file.get('redis', 'broker')
-    BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 15000}
+    broker_url = config_file.get('redis', 'broker')
+    broker_transport_options = {'visibility_timeout': 15000}
 
-    CELERY_TIMEZONE = config_file.get('app', 'timezone')
-    CELERY_RESULT_BACKEND = config_file.get('redis', 'backend')
+    timezone = config_file.get('app', 'timezone')
+    result_backend = config_file.get('redis', 'backend')
     CELERY_REDIS_SCHEDULER_URL = config_file.get('redis', 'scheduler')
 
-    CELERYBEAT_SCHEDULER = 'redisbeat.RedisScheduler'
+    beat_scheduler = 'redisbeat.RedisScheduler'
 
-    CELERYD_TASK_TIME_LIMIT = '2000'
+    task_time_limit = '2000'
